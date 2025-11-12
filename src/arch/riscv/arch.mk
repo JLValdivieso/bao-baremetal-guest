1d338ef432218449be03202e158d4fc5c1246ffe
Bruno Sa <bruno.vilaca.sa@gmail.com>
2025-11-12 00:33:14 +0000

    fix: add sysroot path
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

--- src/arch/riscv/arch.mk
+++ src/arch/riscv/arch.mk
@@ -3,7 +3,7 @@ ARCH_SUB ?= riscv64xcheri
 ifeq ($(ARCH_SUB), riscv64xcheri)
 ARCH_GENERIC_FLAGS = -mcmodel=medany -march=rv64imafdxcheri -mabi=l64pc128 --target=riscv64 --sysroot=$(SYSROOT)/baremetal-newlib-riscv64-purecap/riscv64-unknown-elf
 ARCH_ASFLAGS = --sysroot=$(SYSROOT)/baremetal-newlib-riscv64-purecap/riscv64-unknown-elf
-ARCH_LDFLAGS = --target=riscv64 --sysroot=$(SYSROOT) -lclang_rt.builtins-riscv64 -L$(SYSROOT)/baremetal-riscv64-purecap/lib
+ARCH_LDFLAGS = --target=riscv64 --sysroot=$(SYSROOT)/baremetal-newlib-riscv64-purecap/riscv64-unknown-elf -lclang_rt.builtins-riscv64 -L$(SYSROOT)/baremetal-riscv64-purecap/lib
 else
 ARCH_GENERIC_FLAGS = -mcmodel=medany -march=rv64imac -mabi=lp64
 ARCH_ASFLAGS = 
